055c0ef076e389841e7ed59efbbba18cab10bb7f
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-06-21 11:40:12 +0300

    Реализовал абстрактный класс проверки столкновения со слоями и его наследника который проверяет столкновение в заданной окружности

---
+++ Unit System/LayerContactChecker.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public abstract class LayerContactChecker : MonoBehaviour
+{
+    [Header("Параметры")]
+    [SerializeField] protected LayerMask collisionLayer = 0;
+    
+    [SerializeField]
+    [Tooltip("Смещение точки проверки соприкосновения объекта со слоем")] 
+    protected Vector3 offset = Vector2.zero;
+
+    /// <summary>
+    /// Свойство, которое возвращает позицию точки проверки коллизии transform.position со смещением offset
+    /// </summary>
+    protected Vector3 Pos
+    {
+        get => new Vector3(
+            transform.position.x + offset.x,
+            transform.position.y + offset.y,
+            transform.position.z + offset.z
+        );
+    }
+
+    public abstract bool IsContactedWithLayer();
+}
---
+++ Unit System/LayerContactCheckerSphere.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class LayerContactCheckerSphere : LayerContactChecker
+{
+    [SerializeField] private float radius = 1f;
+
+    public override bool IsContactedWithLayer()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(Pos, radius, collisionLayer);
+        return colliders.Length > 0;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(Pos, radius);
+    }
+}
